3d838040b7f419c933039fdeb03f815d03ff56f9
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2024-04-23 17:04:38 +0545

    updated

--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -8,12 +8,29 @@
 
 
 // another method 
-const arr =[3,5,2]
-let maxNum= arr[0];
-for(let i=0; i<arr.length; i++){
-   if(maxNum<arr[i]){
-    maxNum=arr[i]
-   }
-}
-console.log(maxNum)
+
+// const arr =[3,5,2]
+// let maxNum= arr[0];
+// for(let i=0; i<arr.length; i++){
+//    if(maxNum<arr[i]){
+//     maxNum=arr[i]
+//    }
+// }
+// console.log(maxNum)
+
+//two sum 
+// function twoSum(arr, target){
+//    let index= [];
+//    for(let i=0; i<arr.length; i++){
+//       for(j=i+1; j<arr.length; j++){
+//          if(arr[i]+arr[j]==target){
+//             index.push(arr[i],arr[j])
+//          }
+//       }
+//    }
+//    return index;
+// }
+// console.log(twoSum([2,5,8,4,7,9,6],13))
+
+
 
