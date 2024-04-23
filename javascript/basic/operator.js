b8d14c6e7b6738baae5f151d5bbbc5cbf94c0c93
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2024-04-23 08:59:03 +0545

    updated

--- javascript/basic/operator.js
+++ javascript/basic/operator.js
@@ -8,8 +8,20 @@ const result= x+y;
 
 
 
-//Check below code, output should be false by comparing. console.log the answer
-const a = 10
-const b = '10'
+// //Check below code, output should be false by comparing. console.log the answer
+// const a = 10
+// const b = '10'
 
-console.log(a===b)
+// console.log(a===b)
+
+
+
+
+
+let random = Math.floor(Math.random()*5 +1)
+console.log(random)
+if(random<3){
+  console.log("lesser than 3")
+}else{
+  console.log("greater")
+}
