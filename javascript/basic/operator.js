b2839e5fa8f584a5eb0f04326461360da5d54ee3
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2024-04-23 08:34:03 +0545

    updated

---
+++ javascript/basic/operator.js
@@ -0,0 +1,15 @@
+//Qn1 create two constant ,add them and assign to a new costant
+
+
+const x= 10
+const y=20
+const result= x+y;
+
+
+
+
+//Check below code, output should be false by comparing. console.log the answer
+const a = 10
+const b = '10'
+
+console.log(a===b)
