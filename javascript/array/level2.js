328ea3a410f2f48735dadc3403163fa4421a654b
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2024-04-24 13:07:45 +0545

    updated

--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -18,19 +18,35 @@
 // }
 // console.log(maxNum)
 
+
+
 //two sum 
-// function twoSum(arr, target){
-//    let index= [];
-//    for(let i=0; i<arr.length; i++){
-//       for(j=i+1; j<arr.length; j++){
-//          if(arr[i]+arr[j]==target){
-//             index.push(arr[i],arr[j])
-//          }
-//       }
-//    }
-//    return index;
-// }
-// console.log(twoSum([2,5,8,4,7,9,6],13))
+// return indecies if the sum is equal to target
+
+function twoSum(arr, target){
+   let index= [];
+   for(let i=0; i<arr.length; i++){
+      for(j=i+1; j<arr.length; j++){
+         if(arr[i]+arr[j]==target){
+            index.push(arr[i],arr[j])
+         }
+      }
+   }
+   return index;
+}
+console.log(twoSum([2,5,8,4,7,9,6],13))
 
 
+// Q. Write a function that generates a random alphanumeric string of a given length. 
 
+function randomAlphanumeric(length){
+
+    let char="ABCDEabcde12345";
+    let generatedChar=""
+     for(i=0;i<=length;i++){
+    let randomIndex= Math.floor(Math.random()*char.length+1);
+        generatedChar+=char.charAt(randomIndex) 
+     }
+   return generatedChar
+}
+console.log(randomAlphanumeric(6))
