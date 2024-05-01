858bfff3e6e6bf38d198f5757ce7e1bb11fb325d
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2024-05-01 17:47:52 +0545

    nex Project added

---
+++ react/next01/src/app/page.js
@@ -0,0 +1,13 @@
+import NavBar from "@/components/navbar/NavBar"
+
+
+const page = () => {
+  return (
+    <div>
+      <NavBar/>
+      this is hhome page
+    </div>
+  )
+}
+
+export default page
